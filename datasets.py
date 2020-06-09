caf96e024f1154b8152e511ba3da1301a387e0d5
Vaibhav <vaibhavyadav025@gmail.com>
2020-06-09 17:21:52 +0530

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,117 +1,116 @@
-import scipy.io as io
-import torchvision.transforms as transforms
-import os
+from scipy.io import loadmat
+from os import listdir
 from PIL import Image
+
 import numpy as np
 import torch
-import torch.utils.data as data
+from torch.utils.data import Dataset
 
-class LSPDataset(data.Dataset):
+class LSP(Dataset):
     
-    def __init__(self, path, image_size = 128, hmap_size = 0):
+    def __init__(self, path, transforms, image_size, hmap_size = 0):
         self.image_size = image_size
         self.hmap_size = hmap_size
         self.path = path
+        self.joints = loadmat(self.path + 'joints.mat')['joints'].T
+        self.transforms = transforms
         self.get_paths()
-        self.joints = io.loadmat(self.path + 'joints.mat')['joints'].T
-        self.transform = transforms.Compose([
-            transforms.Resize((self.image_size, self.image_size)),
-            transforms.ToTensor(),
-        ])
         
     def get_paths(self):
-        self.paths = os.listdir(self.path + 'images/')
+        self.paths = listdir(self.path + 'images/')
+        self.paths.sort()
     
     def __len__(self):
         return len(self.paths)
     
     def __getitem__(self, index):
-        image = Image.open(self.path + 'images/' + self.paths[index])
         
-        size = image.size
-        x_scale = self.image_size/size[0]
-        y_scale = self.image_size/size[1]
-        image = self.transform(image)
+        path = self.path + 'images/' + self.paths[index]
+        image, x_scale, y_scale = load_image(path, self.transforms, self.image_size)
         
-        xjoints = (self.joints[index][:,:1] * x_scale)/self.image_size
-        yjoints = (self.joints[index][:,1:2] * y_scale)/self.image_size
-        joints = np.concatenate((xjoints, yjoints), axis=1).reshape(-1)
+        joints, center = get_joints(self.joints[index], (x_scale, y_scale), self.image_size)
 
         mask = torch.tensor([m == 0 for m in self.joints[index][:,2:].reshape(-1)])
-
-        center = sum(xjoints[xjoints>0])/len(xjoints[xjoints>0]), sum(yjoints[yjoints>0])/len(yjoints[yjoints>0])
-
         
         if self.target_type == 'points':
-            return image, torch.from_numpy(joints).float(), center, mask
+            return image, joints, center, mask
 
         if self.target_type == 'heatmap':
             points = joints.reshape(14, 2) * self.hmap_size
-            hmap = gen_hmap(self.hmap_size, self.hmap_size, points, s=1.5)
+            hmap = gen_hmap(self.hmap_size, self.hmap_size, points.numpy(), s=1.5)
             center = gen_hmap(self.hmap_size, self.hmap_size, [[center[0] * self.hmap_size, center[1] * self.hmap_size]], s=1.5)
-            return image, torch.from_numpy(hmap).float(), center, mask
+            return image, hmap, center, mask
 
         
-class LSPExtendedDataset(data.Dataset):
+class LSPet(Dataset):
     
-    def __init__(self, path, image_size = 128, hmap_size = 0):
-        self.path = path
-        self.get_paths()
-        self.joints = io.loadmat(self.path + 'joints.mat')['joints'].T
+    def __init__(self, path, transforms, image_size = (128, 128), hmap_size = 0):
         self.image_size = image_size
         self.hmap_size = hmap_size
-        self.transform = transforms.Compose([
-            transforms.Resize((self.image_size, self.image_size)),
-            transforms.ToTensor(),
-        ])
+        self.path = path
+        self.joints = loadmat(self.path + 'joints.mat')['joints'].T
+        self.transforms = transforms
+        self.get_paths()
         
     def get_paths(self):
-        self.paths = os.listdir(self.path + 'images/')
+        self.paths = listdir(self.path + 'images/')
+        self.paths.sort()
     
     def __len__(self):
         return len(self.paths)
     
     def __getitem__(self, index):
         
-        image = Image.open(self.path + 'images/' + self.paths[index])
-        size = image.size
-        x_scale = self.image_size/size[0]
-        y_scale = self.image_size/size[1]
-        image = self.transform(image)
+        path = self.path + 'images/' + self.paths[index]
+        image, x_scale, y_scale = load_image(path, self.transforms, self.image_size)
         
-        xjoints = (self.joints[index].T[:,:1] * x_scale)/self.image_size
-        yjoints = (self.joints[index].T[:,1:2] * y_scale)/self.image_size
-        joints = np.concatenate((xjoints, yjoints), axis=1).reshape(-1)
+        joints, center = get_joints(self.joints[index], (x_scale, y_scale), self.image_size)
 
-        mask = self.joints[index].T[:,2:]
-        mask = torch.from_numpy(mask).float().reshape(-1)
+        mask = torch.from_numpy(self.joints[index].T[:,2:]).reshape(-1).float()
 
-        center = sum(xjoints[xjoints>0])/len(xjoints[xjoints>0]), sum(yjoints[yjoints>0])/len(yjoints[yjoints>0])
-
-        
         if self.hmap_size == 0:
-            return image, torch.from_numpy(joints).float(), center, mask
+            return image, joints, center, mask
         
         if self.hmap_size > 0:
             points = joints.reshape(14, 2) * self.hmap_size
-            hmap = gen_hmap(self.hmap_size, self.hmap_size, points, s=1.5)
+            hmap = gen_hmap(self.hmap_size, self.hmap_size, points.numpy(), s=1.5)
             center = gen_hmap(self.hmap_size, self.hmap_size, [[center[0] * self.hmap_size, center[1] * self.hmap_size]], s=1.5)
-            return image, torch.from_numpy(hmap).float(), center, mask
+            return image, hmap, center, mask
 
+# load and transform images and return the image and the x, y scaling
+def load_image(path, transforms, size):
+    image = Image.open(path)
+    image_size = image.size
+    image = transforms(image)
+    x_scale = size[0] / image_size[0]
+    y_scale = size[1] / image_size[1]
+    return image, x_scale, y_scale
+
+# get joints and center points
+def get_joints(joints, scales, size):
+    x_joints = (joints.T[:,:1] * scales[0]) / size[0]
+    y_joints = (joints.T[:,1:2] * scales[1]) / size[1]
+    joints = np.concatenate((x_joints, y_joints), axis=1).reshape(-1)
+    joints = torch.from_numpy(joints).float()
+    center = [sum(x_joints[x_joints>0])/len(x_joints[x_joints>0]), sum(y_joints[y_joints>0])/len(y_joints[y_joints>0])]
+    return joints, center
+
+
+# generate gaussian distribution heatmap
 def gaussian(x0, y0, sigma, width, height):
     x = np.arange(0, width, 1, float)
     y = np.arange(0, height, 1, float)[:, np.newaxis]
     return np.exp(-(((x-x0)**2 + (y - y0)**2 )/ (2 * sigma**2)))
 
+# Generate heatmaps for n points
 def gen_hmap(height, width, points, s = 3):
     npoints = len(points)
     hmap = np.zeros((height, width, npoints), dtype = np.float)
     for i in range(npoints):
         if points[i][1] >= 0 and points[i][0] >= 0:
-            if points[i][1] >= height: points[i][1] = height - 1
+            if points[i][0] >= height: points[i][0] = height - 1
             if points[i][1] >= width: points[i][1] = width - 1
             hmap[:,:,i] = gaussian(points[i][1],points[i][0],s,height, width)
         else:
             hmap[:,:,i] = np.zeros((height, width))
-    return hmap.T
-        
+    return torch.from_numpy(hmap.T).float()
